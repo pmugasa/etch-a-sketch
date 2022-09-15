27a7356248b6bd74ed40e1602b8337a48372df6f
Peterson <pmugasa78@gmail.com>
2022-09-15 19:51:34 +0200

    added the grid css style to display a 16x16 grid

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const container = document.querySelector(".container");
 // inside this container we need to create 16 divs
 function divCreate() {
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("inner-div");
     container.appendChild(div);
