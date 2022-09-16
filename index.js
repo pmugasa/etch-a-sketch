c56e0aecaecaa2248b1b615a8bf3b0d06c0ec749
Peterson <pmugasa78@gmail.com>
2022-09-16 19:52:30 +0200

    added the draw function that lets a user draw when a mouse enters the sketch pad

--- index.js
+++ index.js
@@ -5,8 +5,28 @@ function divCreate() {
   for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("inner-div");
+
     container.appendChild(div);
   }
 }
 
 divCreate();
+
+// first we need to get hold of the created divs
+// then attach a mousedown event
+// when the user presses down whilst inside the div, the bg changes
+function draw() {
+  const divs = document.querySelectorAll(".inner-div");
+  divs.forEach((div) => {
+    div.addEventListener("mouseenter", () => {
+      console.log("box clicked");
+      div.setAttribute("style", "background-color: black");
+    });
+  });
+}
+/*
+const divs = document.querySelectorAll(".inner-div");
+divs.forEach((div) => {
+  div.addEventListener("click", draw());
+});*/
+draw()
