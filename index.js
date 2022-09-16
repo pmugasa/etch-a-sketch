50fa6a9c5af83d28150c6bf195cd21c858f4a055
Peterson <pmugasa78@gmail.com>
2022-09-16 22:41:52 +0200

    added a function that takes user input and uses it to create the squares

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-//we need to get hold of the container
-const container = document.querySelector(".container");
-// inside this container we need to create 16 divs
-function divCreate() {
-  for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("inner-div");
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", handleClick);
 
-    container.appendChild(div);
+function handleClick(userInput) {
+  userInput = prompt("Enter number between 8 and 100");
+  console.log(userInput);
+
+  function divCreate() {
+    const container = document.querySelector(".container");
+    for (let i = 0; i < userInput; i++) {
+      const div = document.createElement("div");
+      div.classList.add("inner-div");
+
+      container.appendChild(div);
+    }
   }
+  divCreate();
 }
-
-divCreate();
 
 // first we need to get hold of the created divs
 // then attach a mousedown event
@@ -19,14 +24,19 @@ function draw() {
   const divs = document.querySelectorAll(".inner-div");
   divs.forEach((div) => {
     div.addEventListener("mouseenter", () => {
-      console.log("box clicked");
       div.setAttribute("style", "background-color: black");
     });
   });
 }
-/*
+
 const divs = document.querySelectorAll(".inner-div");
 divs.forEach((div) => {
-  div.addEventListener("click", draw());
-});*/
-draw()
+  div.addEventListener("click", draw);
+});
+
+// when a user clicks the button they enter a number
+// this number is then used to set the number of grids
+// if a user enters a number below 8 they are alerted that its not valid
+// if a number is above 100 they are alerted that it is not valid
+// when a valid number is received it is then used as the number to set the grids
+// the grids have to fit in the box and take up as much space as they can
