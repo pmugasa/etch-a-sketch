82f82a2e8afe3f6d474c9117d7ea624e61897e28
Peterson <pmugasa78@gmail.com>
2022-09-17 22:56:53 +0200

    added a function that takes the user's input and use that input to set the number of grid style

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const btn = document.querySelector(".btn");
 btn.addEventListener("click", handleClick);
 
-function handleClick(userInput) {
-  userInput = prompt("Enter number between 8 and 100");
-  console.log(userInput);
-
-  function divCreate() {
-    const container = document.querySelector(".container");
-    for (let i = 0; i < userInput; i++) {
-      const div = document.createElement("div");
-      div.classList.add("inner-div");
-
-      container.appendChild(div);
-    }
-  }
-  divCreate();
+function handleClick() {
+  const userInput = prompt("Enter a number between 8 and 100");
+  const gridNum = userInput * userInput;
+  gridCreate(gridNum);
+  return;
 }
 
-// first we need to get hold of the created divs
-// then attach a mousedown event
-// when the user presses down whilst inside the div, the bg changes
+function gridCreate(gridNum) {
+  const container = document.querySelector("#container");
+  for (let i = 0; i < gridNum; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("class", "inner-div");
+    container.style.setProperty(
+      "grid-template-columns",
+      "repeat(" + 16 + ", minmax(90px, 1fr)"
+    );
+    container.append(div);
+  }
+}
+
 function draw() {
   const divs = document.querySelectorAll(".inner-div");
   divs.forEach((div) => {
@@ -34,9 +35,6 @@ divs.forEach((div) => {
   div.addEventListener("click", draw);
 });
 
-// when a user clicks the button they enter a number
-// this number is then used to set the number of grids
-// if a user enters a number below 8 they are alerted that its not valid
-// if a number is above 100 they are alerted that it is not valid
-// when a valid number is received it is then used as the number to set the grids
-// the grids have to fit in the box and take up as much space as they can
+//change the number of grids based on the clicked button
+// based on the button clicked change the number of times the loops
+// based on the clicked button change the grid template row and columns
