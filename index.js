afda0b83a9c4165398293e6d6b845b669a64a05d
Peterson <pmugasa78@gmail.com>
2022-09-18 09:42:14 +0200

    changed the css styles in js that lets the grids auto size without overflowing

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ btn.addEventListener("click", handleClick);
 function handleClick() {
   const userInput = prompt("Enter a number between 8 and 100");
   const gridNum = userInput * userInput;
-  gridCreate(gridNum);
+  gridCreate(gridNum, userInput);
   return;
 }
 
-function gridCreate(gridNum) {
+function gridCreate(gridNum, userInput) {
   const container = document.querySelector("#container");
   for (let i = 0; i < gridNum; i++) {
     const div = document.createElement("div");
     div.setAttribute("class", "inner-div");
     container.style.setProperty(
       "grid-template-columns",
-      "repeat(" + 16 + ", minmax(90px, 1fr)"
+      "repeat(" + userInput + ", minmax(auto, 1fr)"
+    );
+    container.style.setProperty(
+      "grid-template-rows",
+      "repeat(" + userInput + ", minmax(auto, 1fr"
     );
     container.append(div);
   }
@@ -29,12 +33,4 @@ function draw() {
     });
   });
 }
-
-const divs = document.querySelectorAll(".inner-div");
-divs.forEach((div) => {
-  div.addEventListener("click", draw);
-});
-
-//change the number of grids based on the clicked button
-// based on the button clicked change the number of times the loops
-// based on the clicked button change the grid template row and columns
+draw();
