dd8c2949604fd84b6cb8a597c885802bc47486eb
Peterson <pmugasa78@gmail.com>
2022-09-20 23:25:19 +0200

    edited the grid function so that it has default values at the beginning

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-// when button is not clicked
-let userInput = 8;
-let gridNum = 64;
-gridCreate(64, 8);
-//change grids button
+//default grids
+gridCreate(64, 8)
+
+startDrawing();
+
+//change grids when button is clicked
 const btn = document.querySelector(".btn");
-
-btn.addEventListener("click", handleClick);
-
-//handling button event
-function handleClick(userInput) {
+btn.addEventListener("click", (userInput, gridNum) => {
   userInput = prompt("Enter a number between 8 and 100");
   gridNum = userInput * userInput;
   if (userInput < 8 || userInput > 100) {
@@ -16,10 +13,15 @@ function handleClick(userInput) {
   } else {
     return gridCreate(gridNum, userInput);
   }
-}
+});
 
 //create grids
 function gridCreate(gridNum, userInput) {
+  const dGrids = Array.from(document.querySelectorAll("default-grid"));
+  dGrids.forEach((dGrid) => {
+    dGrid.classList.remove("default-grid");
+    dGrid.style.display = "none";
+  });
   const container = document.querySelector("#container");
   for (let i = 0; i < gridNum; i++) {
     const div = document.createElement("div");
